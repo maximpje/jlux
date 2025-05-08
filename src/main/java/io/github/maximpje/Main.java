9163d80c28a06be3927e98f6be1696a594249b4b
Maxim Wynant <wynantmaxim@gmail.com>
2025-05-08 23:46:18 +0200

    I shouldve commit earlier too much to note

--- src/main/java/io/github/maximpje/Main.java
+++ src/main/java/io/github/maximpje/Main.java
@@ -12,29 +12,39 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     static boolean hadError = false;
+
+
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.tokens();
-// For now, just print the tokens.
+
         for (Token token : tokens) {
-            System.out.println(token);
+            System.out.println(token); // temporary prints tokens
         }
         if (hadError) System.exit(65);
     }
+
+    // called when an error occurs
     static void error(int line, String message) {
         report(line, "", message);
     }
-    private static void report(int line, String where,
-                               String message) {
+
+    // default error report
+    private static void report(int line, String where, String message) {
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
+
+    // passes a file to the run method
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
     }
+
+    // prompts LOX code and passes to the run method
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
@@ -46,13 +56,16 @@ public class Main {
             hadError = false;
         }
     }
+
+
     public static void main(String[] args) throws IOException{
-        if (args.length > 1) {
+
+        if (args.length > 1) { // exits if multiple args given; only one expected
             System.out.println("Usage: jlox [script]");
             System.exit(64);
-        } else if (args.length == 1) {
+        } else if (args.length == 1) { // passes the given file path to the runFile method
             runFile(args[0]);
-        } else {
+        } else { // prompt LOX code when no args given
             runPrompt();
         }
     }
